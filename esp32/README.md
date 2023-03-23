3386eea9e41e4fe1706eb51b4209e1e030be50bc
Pyrex-ops <60523265+Pyrex-ops@users.noreply.github.com>
2023-03-23 22:03:32 +0100

    Scrittura iniziale pagina mostrata all'utente Seguito dall'issue #7

--- esp32/README.md
+++ esp32/README.md
@@ -11,3 +11,7 @@ Il chip utilizzato, esp32, è montato su diverse schede che di seguito saranno t
 Queste schede seguono questo pinout
 
 ![ESP32_Pinout](https://user-images.githubusercontent.com/60523265/223776226-cc2bbbba-6fa4-40ba-87e2-3c9410aefb5b.png)
+
+## Local Web Server
+
+Per consentire all'utente di inserire le credenziali della propria rete Wi-fi che sarà utilizzata dalla ESP32 per poter accedere alla rete internet, è esposta una rete Wi-Fi gestita dalla ESP32 con un Web Server che fornisce un'interfaccia all'utente per fornire le credenziali richieste.
---
+++ esp32/src/LocalWebServer/LocalWebServer.ino
@@ -0,0 +1,88 @@
+// Load Wi-Fi library
+#include <WiFi.h>
+
+// Replace with your network credentials
+const char* ssid     = "APTs";
+const char* password = "SUGOMASUS";
+
+IPAddress local_IP(192,168,4,1);//Set the IP address of ESP32 itself
+IPAddress gateway(192,168,4,1);   //Set the gateway of ESP32 itself
+IPAddress subnet(255,255,255,0);  //Set the subnet mask for ESP32 itself
+
+// Set web server port number to 80
+WiFiServer server(80);
+
+// Variable to store the HTTP request
+String header;
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.disconnect();
+  WiFi.mode(WIFI_AP);
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Setting AP (Access Point)");
+  // Remove the password parameter, if you want the AP (Access Point) to be open
+  WiFi.softAP(ssid, password);
+
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+  
+  server.begin(80);
+  Serial.println("OK");
+}
+
+void loop(){
+  WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            // Routing nelle pagine
+            if (header.indexOf("GET /") >= 0) {
+              Serial.println("/");
+              //Codice della route qui
+              client.println("<!DOCTYPE html> <html lang=\"it\"> <head> <meta charset=\"UTF-8\"> <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"> <title>Imposta Wi-Fi</title> </head> <body> <h1>Wi-Fi</h1> Seleziona la rete Wi-Fi a cui collegare la scheda. <li>Uno</li> <li>Uno</li> <li>Uno</li> <li>Uno</li> <li>Uno</li> <li>Uno</li> <button>OK</button> </body> </html>");
+            }
+            if (header.indexOf("GET /setPassword") >= 0) {
+              Serial.println("/setPassoword");
+              //Codice della route qui
+              client.println("<!DOCTYPE html><html><h1>AMOGUS</h1></html>");
+            }
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
+}
