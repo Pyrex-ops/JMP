3bc3e27841e7fee1aef9dbee71d4643cef740f18
giovanni <giovanni.francesco11c@gmail.com>
2023-03-27 18:44:26 +0200

    Integrati esempi di utilizzo dell'encoder

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -1,13 +1,18 @@
 #include "src/motorino/Motorino.hpp"
 #include "src/motorino/MotorinoGravity.hpp"
+#include "src/encoder/Encoder.hpp"
+#include "src/encoder/NewEncoderAdapter.hpp"
 #include "constants.h"
 
 Motorino* motorino = new MotorinoGravity(MOTORINO_PIN,MINIMUM_MOTORINO_INTENSITY ,
   MOTORINO_LEDC_CHANNEL , MOTORINO_LEDC_FREQUENCY, MOTORINO_LEDC_RESOLUTION , 
   MOTORINO_TASK_PRIORITY , MOTORINO_TASK_CORE , MOTORINO_TEMPO_FRA_VIBRAZIONI);
 
+Encoder* encoder = new NewEncoderAdapter(ENCODER_CLK_PIN,ENCODER_DT_PIN,ENCODER_PPR);
+
 void setup() {
   Serial.begin(115200);
+  encoder->begin();
   motorino->begin();
 }
 
@@ -15,6 +20,14 @@ void loop() {
   motorino->vibraIntermittente(20,200,3);
   motorino->vibra(500);
   motorino->vibraIntermittente(300,200,5,200);
-  delay(2000);
-  delay(3000);
+  for(int i=0;i<500;i++) {
+    Serial.print(encoder->getPosition());
+    Serial.print(",");
+    Serial.print(encoder->getClockwiseRevolutions());
+    Serial.print(",");
+    Serial.print(encoder->getCounterclockwiseRevolutions());
+    Serial.print(",");
+    Serial.println(encoder->getRevolutions());
+    delay(10);
+  }
 }
