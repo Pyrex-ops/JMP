ef2b54c21c031b5ae4fea94a75255ee64e55a914
Pyrex-ops <60523265+Pyrex-ops@users.noreply.github.com>
2023-04-29 12:48:08 +0200

    Corretta stampa
    Corretta condizione in cui dare errore durante il login

--- web/app/auth/login.php
+++ web/app/auth/login.php
@@ -3,21 +3,26 @@ require_once("../db/dbconnessione.php");
 require_once("../db/sessione.php");
 
 
-if(isset($_POST["username"]) && isset($_POST["password"]) && !$_SESSION["auth"]){
+if (isset($_POST["username"]) && isset($_POST["password"]) && !$_SESSION["auth"]) {
     if (isset($database)) {
+        //TODO: indice su username
         $queryHash = $database->prepare("SELECT passwordhash FROM utente WHERE username = ?");
         $queryHash->bind_param("s", $_POST["username"]);
         $queryHash->execute();
         $arrayHash = $queryHash->get_result()->fetch_assoc();
-        if(password_verify($_POST["password"],$arrayHash["passwordhash"])) {
-            $_SESSION["auth"] = true;
-            //echo "\nLogin eseguito con successo";
-            header("location:../dashboard.php");
-        } else {
-            $_SESSION["auth"] = false;
-            //echo "\nPassword errata";
+        if (isset($arrayHash["passwordhash"])) {
+            if (password_verify($_POST["password"], $arrayHash["passwordhash"])) {
+                $_SESSION["auth"] = true;
+                $_SESSION["erroreLogin"] = false;
+                //echo "\nLogin eseguito con successo";
+                header("location:../dashboard.php");
+            } else {
+                $_SESSION["auth"] = false;
+                $_SESSION["erroreLogin"] = true;
+                //echo "\nPassword errata";
+            }
+            $queryHash->close();
         }
-        $queryHash->close();
 
     }
 
@@ -39,18 +44,16 @@ if(isset($_POST["username"]) && isset($_POST["password"]) && !$_SESSION["auth"])
             <label for="username">
                 Username
                 <input type="text" id="username" name="username" placeholder="Username" required>
-                <small>Verrà richiesto per il login</small>
             </label>
             <label for="password">
                 Password
                 <input type="password" id="password" name="password" placeholder="Password" required>
-                <small>Raccomandazione: Scegli una password forte!</small>
             </label>
         </div>
         <button type="submit">Login</button>
         <?php
         if (isset($_SESSION["auth"])) {
-            if (!$_SESSION["auth"]) {
+            if (isset($_SESSION["erroreLogin"]) && $_SESSION["erroreLogin"]) {
                 echo "Errore";
             }
         }
