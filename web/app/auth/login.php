cacc976641f4c250b1385e7da2e1acd486314535
Giovanni Francesco Comune <giovanni.francesco11c@gmail.com>
2023-05-03 16:02:50 +0200

    aggiunto css bootstrap

--- web/app/auth/login.php
+++ web/app/auth/login.php
@@ -25,38 +25,79 @@ if (isset($_POST["username"]) && isset($_POST["password"]) && !isset($_SESSION["
 
 }
 ?>
-<!doctype html>
-<html lang="it">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">
-    <title>Login</title>
-</head>
-<body>
-<main class="container">
-    <h1>Login</h1>
-    <form action="" method="post">
-        <div class="grid">
-            <label for="username">
-                Username
-                <input type="text" id="username" name="username" placeholder="Username" required>
-            </label>
-            <label for="password">
-                Password
-                <input type="password" id="password" name="password" placeholder="Password" required>
-            </label>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Login | ConfigurazioneCorda</title>
+
+    <!-- Bootstrap CSS -->
+    <link
+      rel="stylesheet"
+      href="/bootstrap.css"
+      crossorigin="anonymous"
+    />
+  </head>
+  <body class="bg-light d-flex flex-column min-vh-100">
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-md navbar-dark bg-primary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">ConfigurazioneCorda.it</a>
+      </div>
+    </nav>
+
+    <!-- Login form -->
+    <div class="container my-5">
+      <div class="row justify-content-center">
+        <div class="col-md-6 col-lg-4">
+          <div class="card">
+            <div class="card-body">
+              <h3 class="card-title text-center mb-4">Login</h3>
+              <form method="post" action="your-url-here">
+                <div class="mb-3">
+                  <label for="username" class="form-label">Username</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="username"
+                    name="username"
+                    required
+                  />
+                </div>
+                <div class="mb-3">
+                  <label for="password" class="form-label">Password</label>
+                  <input
+                    type="password"
+                    class="form-control"
+                    id="password"
+                    name="password"
+                    required
+                  />
+                </div>
+                <div class="text-center">
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-        <button type="submit">Login</button>
-        <?php
-        /*Non funzionante ???
-         * if (isset($_SESSION["auth"])) {
-            if (!$_SESSION["auth"]) {
-                echo "Errore";
-            }
-        }*/
-        ?>
-    </form>
-</main>
-</body>
+      </div>
+    </div>
+
+    <div class="wrapper flex-grow-1"></div>
+    <!-- Footer -->
+    <footer class="bg-primary mt-auto py-3">
+      <div class="container text-center">
+        &copy; ConfigurazioneCorda 2023
+      </div>
+    </footer>
+
+    <!-- Bootstrap JS -->
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-Tt+Fto21Ubk+1G2v01BkFwCYA/kP8WpZldVGvZnX9MzCrAOuA6gY+7VSNyxCeLw7"
+      crossorigin="anonymous"
+    ></script>
+  </body>
 </html>
