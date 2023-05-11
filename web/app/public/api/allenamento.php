a6631d6112c68b2c20d290d4f221a6a7dd622c5c
Pyrex-ops <60523265+Pyrex-ops@users.noreply.github.com>
2023-05-11 20:45:48 +0200

    Risolto errore merge
    Ebbene non era stato completato correttamente il merge del branch
    'database'.
    Corretto tipo del dato passato alla query.

--- web/app/public/api/allenamento.php
+++ web/app/public/api/allenamento.php
@@ -7,17 +7,12 @@ if (isset($_GET['id'])) {
     //Da id dispositivo ricavare utente e aggiungere un nuovo allenamento
     if (isset($database)) {
         $queryDispositivo = $database->prepare('SELECT IDDispositivo FROM dispositivo WHERE IDDispositivo = (?)');
-        $queryDispositivo->bind_param('i', $_GET['id']);
+        $queryDispositivo->bind_param('s', $_GET['id']);
         $queryDispositivo->execute();
         if (! $queryDispositivo->get_result()->num_rows == 0) {
             $queryDispositivo->close();
-<<<<<<< HEAD:web/app/api/allenamento.php
-            $queryUtente = $database->prepare("SELECT utente.IDUtente FROM utente JOIN dispositivo ON utente.IDUtente = dispositivo.IDUtente WHERE IDDispositivo = (?) LIMIT 1");
-            $queryUtente->bind_param("s", $_GET["id"]);
-=======
             $queryUtente = $database->prepare('SELECT utente.IDUtente FROM utente JOIN dispositivo ON utente.IDUtente = dispositivo.IDDispositivo WHERE IDDispositivo = (?) LIMIT 1');
             $queryUtente->bind_param('s', $_GET['id']);
->>>>>>> e2c8ce5daa4a3ce3ab6407ed2f918cc6f3f34fcc:web/app/public/api/allenamento.php
             $queryUtente->execute();
             $temp = $queryUtente->get_result()->fetch_assoc();
             $userID = $temp['IDUtente']; //TODO: Sarà unico. Trova un modo più pulito. Vedi se aggregare tutto in unica query (quella di sopra in questa qui sotto)
--- web/app/public/api/getClassifica.php
+++ web/app/public/api/getClassifica.php
@@ -7,20 +7,9 @@ if (isset($database)) {
     if (isset($_GET['tipo'])) {
         if ($_GET['tipo'] == 1) {
             //Numero salti utente
-<<<<<<< HEAD:web/app/api/getClassifica.php
             //TODO: Prendi i dati dalle view
             $numSalti = $database->query("SELECT * FROM classificanumsalti");
             $arraySalti = array();
-=======
-            $numSalti = $database->query('SELECT utente.username, SUM(valore) as numSalti 
-                                        FROM misura
-                                            JOIN allenamento ON misura.IDAllenamento = allenamento.IDAllenamento
-                                            JOIN utente ON allenamento.IDUtente = utente.IDUtente
-                                        GROUP BY allenamento.IDAllenamento
-                                        ORDER BY numSalti DESC;
-                                ');
-            $arraySalti = [];
->>>>>>> e2c8ce5daa4a3ce3ab6407ed2f918cc6f3f34fcc:web/app/public/api/getClassifica.php
             while ($riga = $numSalti->fetch_assoc()) {
                 $arraySalti[] = $riga;
             }
@@ -29,20 +18,9 @@ if (isset($database)) {
             exit;
         } elseif ($_GET['tipo'] == 2) {
             //Durata allenamento
-<<<<<<< HEAD:web/app/api/getClassifica.php
             $durataAllenamenti = $database->query("SELECT * FROM classificadurata");
 
             $arrayDurata = array();
-=======
-            $durataAllenamenti = $database->query('SELECT utente.username, UNIX_TIMESTAMP(MAX(timestamp)) - UNIX_TIMESTAMP(MIN(timestamp)) durataAllenamento
-                                                 FROM misura
-                                                    JOIN allenamento ON misura.IDAllenamento = allenamento.IDAllenamento
-                                                    JOIN utente ON allenamento.IDUtente = utente.IDUtente
-                                                 GROUP BY misura.IDAllenamento
-                                                 ORDER BY durataAllenamento DESC;
-                                        ');
-            $arrayDurata = [];
->>>>>>> e2c8ce5daa4a3ce3ab6407ed2f918cc6f3f34fcc:web/app/public/api/getClassifica.php
             while ($riga = $durataAllenamenti->fetch_assoc()) {
                 $arrayDurata[] = $riga;
             }
