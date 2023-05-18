37e1dd0f048d13473ef37a0f3b19c1c3a422decb
Pyrex-ops <60523265+Pyrex-ops@users.noreply.github.com>
2023-05-18 10:24:15 +0200

    Aggiunto controllo per l'allenamento.

--- web/app/public/api/allenamento.php
+++ web/app/public/api/allenamento.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once "/php/private/model/db/dbconnessione.php";
+require_once "../../private/model/db/dbconnessione.php";
 header('Content-Type: application/json');
 
 if (isset($_GET['id'])) {
@@ -15,37 +15,44 @@ if (isset($_GET['id'])) {
             $queryUtente->bind_param('s', $_GET['id']);
             $queryUtente->execute();
             $temp = $queryUtente->get_result()->fetch_assoc();
-            $userID = $temp['IDUtente']; //TODO: Sarà unico. Trova un modo più pulito. Vedi se aggregare tutto in unica query (quella di sopra in questa qui sotto)
-            //Controlliamo il timestamp dell'ultimo allenamento, se è troppo recente allora non inseriamo un nuovo allenamento
-            $queryControllo = $database->query("SELECT timestamp FROM misura JOIN allenamento ON misura.IDAllenamento = allenamento.IDAllenamento WHERE allenamento.IDUtente = $userID AND TIMESTAMPDIFF(SECOND, timestamp, NOW()) < 30 ORDER BY timestamp DESC LIMIT 1;");
-            $queryControlloAllenamentoVuoto = $database->query("SELECT COUNT(misura.IDMisura) as c
+            if (isset($temp)) {
+                $userID = $temp['IDUtente']; //TODO: Sarà unico. Trova un modo più pulito. Vedi se aggregare tutto in unica query (quella di sopra in questa qui sotto)
+                //Controlliamo il timestamp dell'ultimo allenamento, se è troppo recente allora non inseriamo un nuovo allenamento
+                $queryControllo = $database->query("SELECT timestamp FROM misura JOIN allenamento ON misura.IDAllenamento = allenamento.IDAllenamento WHERE allenamento.IDUtente = $userID AND TIMESTAMPDIFF(SECOND, timestamp, NOW()) < 30 ORDER BY timestamp DESC LIMIT 1;");
+                $queryControlloAllenamentoVuoto = $database->query("SELECT COUNT(misura.IDMisura) as c
 FROM misura
 WHERE misura.IDAllenamento IN (SELECT MAX(allenamento.IDAllenamento)
                                 FROM allenamento JOIN utente ON allenamento.IDUtente = utente.IDUtente
                                     
                                 WHERE utente.IDUtente = $userID
                                 ORDER BY allenamento.IDAllenamento DESC);");
-            $risultatoQueryControllo = $queryControllo->fetch_assoc();
-            $risultatoQueryAllenamentoVuoto = $queryControlloAllenamentoVuoto->fetch_assoc();
-            //Alternativa: inserire allenamento alla creazione dell'utente
-            //Se l'utente non ha allenamenti, facciamo inserire un allenamento.
-            $queryAllenamentiVuoti = $database->query("SELECT allenamento.IDAllenamento FROM allenamento JOIN utente ON allenamento.IDUtente = utente.IDUtente WHERE allenamento.IDUtente = $userID ORDER BY allenamento.IDAllenamento DESC LIMIT 1;");
-            if ($queryAllenamentiVuoti->num_rows == 0) {
-                $queryAllenamento = $database->query("INSERT INTO allenamento(IDUtente) VALUE ('$userID')");
-                http_response_code(200);
-                echo json_encode(['stato' => 'ok']);
-                exit;
-            }
-            if (!isset($risultatoQueryControllo["timestamp"]) && $risultatoQueryAllenamentoVuoto["c"] != 0) {
-                $queryAllenamento = $database->query("INSERT INTO allenamento(IDUtente) VALUE ('$userID')");
-                http_response_code(200);
-                echo json_encode(['stato' => 'ok']);
-                exit;
+                $risultatoQueryControllo = $queryControllo->fetch_assoc();
+                $risultatoQueryAllenamentoVuoto = $queryControlloAllenamentoVuoto->fetch_assoc();
+                //Alternativa: inserire allenamento alla creazione dell'utente
+                //Se l'utente non ha allenamenti, facciamo inserire un allenamento.
+                $queryAllenamentiVuoti = $database->query("SELECT allenamento.IDAllenamento FROM allenamento JOIN utente ON allenamento.IDUtente = utente.IDUtente WHERE allenamento.IDUtente = $userID ORDER BY allenamento.IDAllenamento DESC LIMIT 1;");
+                if ($queryAllenamentiVuoti->num_rows == 0) {
+                    $queryAllenamento = $database->query("INSERT INTO allenamento(IDUtente) VALUE ('$userID')");
+                    http_response_code(200);
+                    echo json_encode(['stato' => 'ok']);
+                    exit;
+                }
+                if (!isset($risultatoQueryControllo["timestamp"]) && $risultatoQueryAllenamentoVuoto["c"] != 0) {
+                    $queryAllenamento = $database->query("INSERT INTO allenamento(IDUtente) VALUE ('$userID')");
+                    http_response_code(200);
+                    echo json_encode(['stato' => 'ok']);
+                    exit;
+                } else {
+                    //Assoceremo le nuove misure ad un allenamento precedente che era vuoto
+                    //TODO: aggiungi questa informazione nel JSON
+                    http_response_code(200);
+                    echo json_encode(['stato' => 'ok']);
+                    exit;
+                }
             } else {
-                //Assoceremo le nuove misure ad un allenamento precedente che era vuoto
-                //TODO: aggiungi questa informazione nel JSON
-                http_response_code(200);
-                echo json_encode(['stato' => 'ok']);
+
+                http_response_code(401);
+                echo json_encode(['stato' => 'errore']);
                 exit;
             }
         } else {
