9a56a2a81b8997b65781c9efff5007bd411975e3
Giovanni Francesco Comune <giovanni.francesco11c@gmail.com>
2023-05-25 22:55:34 +0200

    aggiunto js bootstrap a landing page

--- web/app/public/index.php
+++ web/app/public/index.php
@@ -79,7 +79,7 @@ redirect_to_dashboard_if_logged_in() ?>
   <!-- Bootstrap JS -->
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
